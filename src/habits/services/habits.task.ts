02f1a1841e3ba03307c0a23a55f25e354f782ff1
Matthieu <matthieu.delarue5@gmail.com>
2025-02-02 18:06:57 +0100

    🐛 fix moment import syntax across multiple services

--- src/habits/services/habits.task.ts
+++ src/habits/services/habits.task.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import * as moment from 'moment';
+import moment from 'moment';
 import { StatsService } from '../../stats/stats.service';
 import { HabitDocument, HabitType } from '../habits.schema';
 
